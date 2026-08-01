d6cf7b2b3a01c8637d8c315f0ce737fd75f14731
sebasjq <est.juan.ssanchezq@unimilitar.edu.co>
2026-07-31 22:41:30 -0500

    Final comments

--- src/controllers/stores.js
+++ src/controllers/stores.js
@@ -2,8 +2,8 @@ const storesModel = require('../models/stores');
 
 // Controller function to get all stores
 function listAllStores(req, res) {
-    const stores = storesModel.getAllStores();
-    res.json(stores);
+    const stores = storesModel.getAllStores(); // Call the model function to get all stores
+    res.json(stores); // Send the list of stores as a JSON response
 }
 
 // Export the controller functions to be used in the routes
